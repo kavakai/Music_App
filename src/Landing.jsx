01affd90084d2f405c2faed1a9ba1aaeb1324f0f
Kai Kuller <kavakai@gmail.com>
2024-02-03 15:30:14 -0700

    Add search function to landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -9,39 +9,57 @@ import Typography from '@mui/material/Typography';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
+import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+
 
 export default function Landing() {
   const theme = useTheme();
-
+  const [searchValue, setSearchValue] = useState('');
+  console.log(searchValue, "searchValue")
+  
   return (
-    <Card sx={{ display: 'flex', width: '85%' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Mac Miller
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between' }}>
+        <TextField 
+          fullWidth 
+          id="outlined-basic" 
+          label="Enter an Artist Name" 
+          variant="outlined" 
+          value={searchValue}
+          onChange={(event) => {
+            setSearchValue(event.target.value);
+          }}  
+          sx={{ backgroundColor: '#858181', marginBottom: '10px' }}
+        />
+      <Card sx={{ display: 'flex', width: '85%' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              Live From Space
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              Mac Miller
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            <IconButton aria-label="previous">
+              {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+            </IconButton>
+            <IconButton aria-label="next">
+              {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
-    </Card>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image="/static/images/cards/live-from-space.jpg"
+          alt="Live from space album cover"
+        />
+      </Card>
+    </Box>
   );
 }
