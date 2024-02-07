13ca45faeea2af8f2290ae945bd855b71b7ecf32
Kai Kuller <kavakai@gmail.com>
2024-02-07 11:00:55 -0700

    Add spotify fetch client

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import Login from './Login'
 import Landing from './Landing';
+import { redirectToAuthCodeFlow, getAccessToken } from "./fetch_api/Fetch";
+
 
 function App() {
+
+const clientId = import.meta.env.VITE.CLIENT.ID;
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function fetchProfile(code){
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${code}` }
+    });
+
+    return await result.json();
+}
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
